a4f9c31cc5b2350ecd0b9e5245542a1aa17a3fe4
acromtech <agib31@gmail.com>
2025-02-23 22:08:38 +0100

    clean project repertories & add uv installation though setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Mettre à jour le système
-echo "Mise à jour du système..."
+echo "System updates..."
 sudo apt update && sudo apt upgrade -y
 
-# Installer Python et pip (dernière version)
-echo "Installation de Python et pip..."
-sudo apt install -y python3 python3-pip python3-picamera2
+echo "uv project installations..."
+sudo snap install astral-uv --classic
+uv sync 
 
-# Installer des utilitaires et bibliothèques nécessaires
-echo "Installation des bibliothèques et outils nécessaires..."
-sudo apt install -y libasound-dev libportaudio2 libportaudiocpp0 portaudio19-dev \
+echo "Dependencies installation..."
+sudo apt install -y 
+    python3-picamera2 \
+    libasound-dev \
+    libportaudio2 \
+    libportaudiocpp0 \
+    portaudio19-dev \
     ffmpeg \
     python3-dev \
     libsndfile1
 
+echo "Download & Install Vosk (fr) models..."
 cd ./software/lib
 wget https://alphacephei.com/vosk/models/vosk-model-fr-0.22.zip
 unzip vosk-model-fr-0.22.zip
@@ -22,7 +26,7 @@ rm -f vosk-model-fr-0.22.zip
 mv vosk-model-fr-0.22 vosk_model
 
 # Configurer le SPI
-echo "Activation des modules SPI..."
+echo "Activate SPI modules..."
 if ! grep -q "^dtparam=spi=on" /boot/config.txt; then
     echo "dtparam=spi=on" | sudo tee -a /boot/config.txt
 fi
@@ -35,17 +39,14 @@ if ! grep -q "^dtoverlay=spi1-2cs" /boot/config.txt; then
     echo "dtoverlay=spi1-2cs" | sudo tee -a /boot/config.txt
 fi
 
-# Activer l'accès SSH
-echo "Activation de l'accès SSH..."
+echo "SSH activation..."
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
-# Redémarrer pour appliquer les modifications
-echo "Installation terminée. Un redémarrage est nécessaire pour appliquer les modifications."
-echo "Voulez-vous redémarrer maintenant ? (y/n)"
+echo "All packages installed. Reboot now (y/n)?"
 read -r restart_choice
 if [[ $restart_choice == "y" || $restart_choice == "Y" ]]; then
     sudo reboot
 else
-    echo "Redémarrez manuellement pour appliquer les modifications."
+    echo "Reboot needed to apply modifications"
 fi
